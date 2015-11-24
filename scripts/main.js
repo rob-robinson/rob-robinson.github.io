1212acdc7ff5ae830b13b5bea8412454de147a6e
Rob Robinson <robrobinsoncsuf@gmail.com>
2015-11-24 00:51:55 -0800

    add bootsrap and other files

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $(".scroll-top").click(function () {
+    $(".scroll-top").click( function () {
         $("body,html").animate({scrollTop: 0}, 800)
-    }), $(".scroll-down").click(function () {
+            }), $(".scroll-down").click(function () {
         $("body,html").animate({scrollTop: $(window).scrollTop() + 800}, 1e3)
     }), $("body").scrollspy({target: "#navbar"}), $("body").on("click", "#main_nav a", function (o) {
         var t = $(this);
         $("html, body").stop().animate({scrollTop: $(t.attr("href")).offset().top}, 1500, "easeInOutExpo"), o.preventDefault()
-    }), $(window).stellar()
+    }), false //$(window).stellar()
 });
